8178b81b4e20540c95e83d81d223e2f9fa343069
Anthony Liatsis <anthony@relateiq.com>
2015-11-13 13:31:38 -0800

    _isPillerModelValue

--- src/pillerModelValue/index.js
+++ src/pillerModelValue/index.js
@@ -1,101 +1,102 @@
 module.exports = pillerModelValue;
 
 function pillerModelValue(updateFn, text, pills) {
-  var instance = {
-    _pills: [],
-    text: text || ''
-  };
+    var instance = {
+        _isPillerModelValue: true,
+        _pills: [],
+        text: text || ''
+    };
 
-  instance.clearPills = clearPills.bind(instance, updateFn);
-  instance.setPills = setPills.bind(instance, updateFn);
-  instance.addPill = addPill.bind(instance);
-  instance.removePill = removePill.bind(instance);
-  instance.getPills = getPills.bind(instance);
+    instance.clearPills = clearPills.bind(instance, updateFn);
+    instance.setPills = setPills.bind(instance, updateFn);
+    instance.addPill = addPill.bind(instance);
+    instance.removePill = removePill.bind(instance);
+    instance.getPills = getPills.bind(instance);
 
-  instance._updateRanges = updateRanges.bind(instance);
+    instance._updateRanges = updateRanges.bind(instance);
 
-  instance.setPills(pills);
+    instance.setPills(pills);
 
-  return instance;
+    return instance;
 }
 
 function getPills() {
-  return this._pills;
+    return this._pills;
 }
 
 function setPills(updateFn, pills) {
-  if (Array.isArray(pills)) {
-    this._pills = sortPills(pills.slice());
-    updateFn();
-  } else {
-    this.clearPills();
-  }
+    if (Array.isArray(pills)) {
+        this._pills = sortPills(pills.slice());
+        updateFn();
+    } else {
+        this.clearPills();
+    }
 }
 
 function addPill(pill, optionalEndIndex) {
-  if (~this._pills.indexOf(pill)) {
-    pill = pill.clone();
-  }
+    if (~this._pills.indexOf(pill)) {
+        pill = pill.clone();
+    }
 
-  if (typeof pill.positionStart !== 'number' || pill.positionStart > this.text.length) {
-    pill.positionStart = this.text.length;
-  }
+    if (typeof pill.positionStart !== 'number' || pill.positionStart > this.text.length) {
+        pill.positionStart = this.text.length;
+    }
 
-  if (typeof optionalEndIndex !== 'number') {
-    optionalEndIndex = pill.positionStart;
-  }
+    if (typeof optionalEndIndex !== 'number') {
+        optionalEndIndex = pill.positionStart;
+    }
 
-  var insertedText = pill.text + pill.suffix;
-  var toEndOfNewVal = this.text.substring(0, pill.positionStart) + insertedText;
+    var insertedText = pill.text + pill.suffix;
+    var toEndOfNewVal = this.text.substring(0, pill.positionStart) + insertedText;
 
-  this.text = toEndOfNewVal + this.text.substring(optionalEndIndex);
-  this._updateRanges(pill.positionStart, optionalEndIndex, insertedText.length, pill);
+    this.text = toEndOfNewVal + this.text.substring(optionalEndIndex);
+    this._updateRanges(pill.positionStart, optionalEndIndex, insertedText.length, pill);
 
-  return toEndOfNewVal.length + pill.caretPositionFromEnd;
+    return toEndOfNewVal.length + pill.caretPositionFromEnd;
 }
 
 function removePill(pill) {
-  var pillIndex = this._pills.indexOf(pill);
+    var pillIndex = this._pills.indexOf(pill);
 
-  if (~pillIndex) {
-    this._pills.splice(pillIndex, 1);
-    this.text = this.text.substring(0, pill.positionStart) + this.text.substring(pill.positionEnd);
-    this._updateRanges(pill.positionStart, pill.positionEnd, 0);
-  }
+    if (~pillIndex) {
+        this._pills.splice(pillIndex, 1);
+        this.text = this.text.substring(0, pill.positionStart) + this.text.substring(pill.positionEnd);
+        this._updateRanges(pill.positionStart, pill.positionEnd, 0);
+    }
 }
 
 function clearPills(updateFn) {
-  if (this._pills.length) {
-    this._pills.length = 0;
-    updateFn();
-  }
+    if (this._pills.length) {
+        this._pills.length = 0;
+        updateFn();
+    }
 }
 
 function sortPills(pills) {
-  return pills.sort(function(a, b) {
-    return a.positionStart - b.positionStart;
-  });
+    return pills.sort(function(a, b) {
+        return a.positionStart - b.positionStart;
+    });
 }
 
 function updateRanges(changeStart, changeEnd, insertionCount, insertedPill) {
-  var indexDelta = changeStart - changeEnd + (insertionCount || 0);
-  var retainedPills = [];
+    var indexDelta = changeStart - changeEnd + (insertionCount || 0);
+    var retainedPills = [];
 
-  this._pills.forEach(function(pill) {
-    //update position for and retain pill when the change ends before it
-    if (changeEnd <= pill.positionStart) {
-      pill.positionStart = pill.positionStart + indexDelta;
-      retainedPills.push(pill);
+    this._pills.forEach(function(pill) {
+        //update position for and retain pill when the change ends before it
+        if (changeEnd <= pill.positionStart) {
+            pill.positionStart = pill.positionStart + indexDelta;
+            retainedPills.push(pill);
+        }
+        //retain pill when the change starts after it
+        else if (changeStart >= pill.positionEnd) {
+            retainedPills.push(pill);
+        }
+    });
+
+    if (insertedPill) {
+        retainedPills.push(insertedPill);
     }
-    //retain pill when the change starts after it
-    else if (changeStart >= pill.positionEnd) {
-      retainedPills.push(pill);
-    }
-  });
 
-  if (insertedPill) {
-    retainedPills.push(insertedPill);
-  }
-
-  this.setPills(retainedPills);
+    this.setPills(retainedPills);
 }
